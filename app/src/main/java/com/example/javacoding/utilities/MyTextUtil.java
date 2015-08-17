12975c7c605ecf0ffd3d947e35c70dfd18adb587
jackBH <923406823@qq.com>
2015-08-17 15:20:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/javacoding/utilities/MyTextUtil.java
@@ -0,0 +1,10 @@
+package com.example.javacoding.utilities;
+
+/**
+ * Created by Administrator on 2015/8/17.
+ */
+public class MyTextUtil {
+    public String getValue() {
+        return "Hello from the utility class";
+    }
+}
